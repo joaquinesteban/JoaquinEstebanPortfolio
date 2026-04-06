93b5cc25035e540b2fc1eedd7dbe75f6a5e24847
Joaquin Esteban <joaquinesteban84@gmail.com>
2026-04-05 21:18:22 -0300

    Comenzando con el boton hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import CopyButtonCv from "./CopyButtonMail";
 import DownloadCvButton from "./DownloadCvButton";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header id="home" className="px-10 pt-8">
       <nav className="flex justify-between items-center text-sm text-neutral-600">
@@ -10,6 +13,8 @@ const Header = () => {
           <CopyButtonCv text={"joaquinesteban84@gmail.com"} />
           <DownloadCvButton />
         </div>
+
+        <h1 className="md:hidden font-medium">Joaquin</h1>
 
         <div className="flex gap-4 ">
           <a href="https://www.linkedin.com/in/joaquin-esteban-23189920b/">
@@ -20,7 +25,38 @@ const Header = () => {
           </a>
           <a href="#">Instagram</a>
         </div>
+        <button className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+          {open ? "✕" : "☰"}
+        </button>
       </nav>
+      {open && (
+        <div className="absolute top-20 left-0 w-full bg-white shadow-md flex flex-col items-center gap-6 py-6 text-neutral-700 md:hidden">
+          {/* EMAIL */}
+          <span className="font-medium">joaquinandres1718@gmail.com</span>
+
+          <CopyButtonCv text={"joaquinesteban84@gmail.com"} />
+          <DownloadCvButton />
+
+          {/* REDES */}
+          <a
+            href="https://www.linkedin.com/in/joaquin-esteban-23189920b/"
+            onClick={() => setOpen(false)}
+          >
+            LinkedIn
+          </a>
+
+          <a
+            href="https://github.com/joaquinesteban"
+            onClick={() => setOpen(false)}
+          >
+            GitHub
+          </a>
+
+          <a href="#" onClick={() => setOpen(false)}>
+            Instagram
+          </a>
+        </div>
+      )}
     </header>
   );
 };
