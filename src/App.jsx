b2cf1d8933cfae7bf10805813087e791c4afbceb
Joaquin Esteban <joaquinesteban84@gmail.com>
2026-05-02 11:40:17 -0300

    Acomodamos la card para que mobile no se vean los bordes de la card-header queda incomodo para el usuario

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-[#aaaaab] flex justify-center py-10">
-        <div className="w-full max-w-6xl bg-neutral-100 rounded-[40px] shadow-sm overflow-hidden">
+      <div className="min-h-screen bg-[#aaaaab] flex justify-center md:py-10">
+        <div className="w-full bg-[#f5f5f5] overflow-hidden md:max-w-6xl md:mx-auto md:bg-neutral-100 md:rounded-[40px] md:shadow-sm">
           <Header />
           <main>
             <Hero />
